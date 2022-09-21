4a16e1adf46a57e8853cfea02e0e0f09a3b2112b
siyeonSon <siyeon0209@naver.com>
2022-09-21 18:38:32 +0900

    initial commit

---
+++ src/test/java/com/capstone/projectory/ProjectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.projectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
