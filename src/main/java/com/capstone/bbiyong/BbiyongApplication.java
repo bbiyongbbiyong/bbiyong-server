8f2069996f4ccd365aef0645af09280330aa55e1
siyeonSon <siyeon0209@naver.com>
2022-11-18 20:00:38 +0900

    setting: projectory -> bbiyong
    - 프로젝트 주제 변경에 따라 이전 프로젝트 관련 설정들 수정

--- src/main/java/com/capstone/bbiyong/BbiyongApplication.java
+++ src/main/java/com/capstone/bbiyong/BbiyongApplication.java
@@ -1,4 +1,4 @@
-package com.capstone.bbiyongbbiyong;
+package com.capstone.bbiyong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BbiyongApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BbiyongApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BbiyongApplication.class, args);
+    }
 
 }
+
