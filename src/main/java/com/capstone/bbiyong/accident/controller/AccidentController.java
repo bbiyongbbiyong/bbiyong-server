0f7418a2c9a4d1bdd5feae901bc4e54e4a063e47
Jimin0304 <pjws0304@naver.com>
2022-12-02 20:18:15 +0900

    feat(accident): accident api scheduler 구현 및 date 형식 변경 (#12)
    - 1분 간격으로 scheduler를 돌려 새로운 정보가 있으면 받아오게 함
    - 기존 startDate와 endDate가 제대로 받아지지 않아 수정하고, date뿐만 아니라 time도 함꼐 받아오게 함

--- src/main/java/com/capstone/bbiyong/accident/controller/AccidentController.java
+++ src/main/java/com/capstone/bbiyong/accident/controller/AccidentController.java
@@ -1,9 +1,10 @@
 package com.capstone.bbiyong.accident.controller;
 
-import com.capstone.bbiyong.accident.dto.AccidentResponseDTO;
 import com.capstone.bbiyong.accident.service.AccidentService;
 import com.capstone.bbiyong.openapi.OpenAPI;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,6 +15,7 @@ import java.text.ParseException;
 
 import static org.springframework.http.HttpStatus.OK;
 
+@EnableScheduling
 @RestController
 @RequestMapping("accident")
 @RequiredArgsConstructor
@@ -22,6 +24,7 @@ public class AccidentController {
     private final AccidentService accidentService;
     private final OpenAPI accidentOpenAPI;
 
+    @Scheduled(fixedDelay = 60000)  // 1분 간격
     @GetMapping("")
     @ResponseStatus(OK)
     public void callAccidentOpenAPI() throws IOException, ParseException {
