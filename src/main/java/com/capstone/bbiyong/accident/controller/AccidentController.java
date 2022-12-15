0b6b48a635f5a04831492d61cf8e8d01f20c1942
Jimin0304 <pjws0304@naver.com>
2022-12-16 01:15:32 +0900

    feat(accident): 구 단위 accident 정보 조회 api 구현 (#16)

--- src/main/java/com/capstone/bbiyong/accident/controller/AccidentController.java
+++ src/main/java/com/capstone/bbiyong/accident/controller/AccidentController.java
@@ -1,18 +1,18 @@
 package com.capstone.bbiyong.accident.controller;
 
+import com.capstone.bbiyong.accident.dto.AccidentResponseDTO;
 import com.capstone.bbiyong.accident.service.AccidentService;
 import com.capstone.bbiyong.openapi.OpenAPI;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.List;
 
 import static org.springframework.http.HttpStatus.OK;
 
@@ -20,6 +20,7 @@ import static org.springframework.http.HttpStatus.OK;
 @RestController
 @RequestMapping("accident")
 @RequiredArgsConstructor
+@Tag(name = "🚗️ Accident ", description = "사건/사고 API")
 public class AccidentController {
 
     private final AccidentService accidentService;
@@ -29,7 +30,13 @@ public class AccidentController {
     @GetMapping("")
     @ResponseStatus(OK)
     @Operation(summary = "사건/사고 및 시위 API 자동 호출", description = "1분마다 사건/사고 및 시위 정보를 업데이트하여 DB에 저장합니다")
-    public void callAccidentOpenAPI() throws IOException, ParseException {
+    public void callAccidentOpenAPI() throws IOException, ParseException, org.json.simple.parser.ParseException {
         accidentOpenAPI.call();
+    }
+
+    @GetMapping("/{locationId}")
+    @Operation(summary = "지역 별 사건/사고 조회", description = "지역 별 사건/사고를 조회합니다")
+    public List<AccidentResponseDTO> getAccident(@PathVariable("locationId") Long locationId) {
+        return accidentService.getAccident(locationId);
     }
 }
--- src/main/java/com/capstone/bbiyong/accident/repository/AccidentRepository.java
+++ src/main/java/com/capstone/bbiyong/accident/repository/AccidentRepository.java
@@ -1,10 +1,20 @@
 package com.capstone.bbiyong.accident.repository;
 
 import com.capstone.bbiyong.accident.domain.Accident;
+import com.capstone.bbiyong.emerMsg.domain.EmerMsg;
+import com.capstone.bbiyong.location.domain.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 public interface AccidentRepository extends JpaRepository<Accident, Long> {
     Optional<Accident> findByOpenapiId(Long openapiId);
+
+    @Query(value = "select a from Accident a where a.location = :location " +
+            "and a.startDate <= :now and a.endDate >= :now")
+    List<Accident> findAllAccidentByLocationAndDate(@Param("location") Location location, @Param("now") Date now);
 }
