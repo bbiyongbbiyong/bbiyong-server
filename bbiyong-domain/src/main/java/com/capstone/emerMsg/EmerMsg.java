df1c7f267ba2d0dd247552ef054e0bf01df37f4d
siyeonSon <siyeon0209@naver.com>
2023-07-12 17:47:16 +0900

    feat(domain): bbiyong-domain 모듈에 도메인 추가

---
+++ bbiyong-domain/src/main/java/com/capstone/emerMsg/EmerMsg.java
@@ -0,0 +1,47 @@
+package com.capstone.emerMsg;
+
+import com.capstone.common.DateTimeEntity;
+import com.capstone.location.Location;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class EmerMsg extends DateTimeEntity {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "emer_msg_id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long openapiId;
+
+    @Column(nullable = false)
+    private String message;
+
+    @Column(nullable = false)
+    private Date startDate;
+
+    @Column(nullable = false)
+    private Date endDate;
+
+    @ManyToOne
+    @JoinColumn(name = "location_id")
+    private Location location;
+
+    @Builder
+    public EmerMsg(Long openapiId, String message, Date startDate, Date endDate, Location location) {
+        this.openapiId = openapiId;
+        this.message = message;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.location = location;
+    }
+}
