7f7dfd9f98331dffc76af1607da0ed5318c2daf9
Jimin0304 <pjws0304@naver.com>
2023-07-21 17:24:58 +0900

    fix: jar파일 실행 안됨 이슈로 deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,17 +3,19 @@
 REPOSITORY=/home/ubuntu/bbiyong
 cd $REPOSITORY
 
-APP_NAME=bbiyong
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'bbiyong-plain.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-CURRENT_PID=$(pgrep -f $APP_NAME)
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-if [ -z $CURRENT_PID ]
-then
-  echo "> 종료할 애플리케이션이 없습니다."
+CURRENT_PID=$(pgrep -fl action | grep java | awk '{print $1}')
+
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -9 $CURRENT_PID"
+  echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
