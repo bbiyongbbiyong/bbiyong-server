79e5af2d5cc97aab1d969655a63af139779e074d
siyeonSon <siyeon0209@naver.com>
2023-07-26 17:38:48 +0900

    feature: member 테이블 생성

---
+++ bbiyong-domain/src/main/java/com/capstone/member/domain/Member.java
@@ -0,0 +1,31 @@
+package com.capstone.member.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    // TODO: List<Token> 생성
+
+}
