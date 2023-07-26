eff37a41fb322d7af8db7b1bf40de4be8cf15962
siyeonSon <siyeon0209@naver.com>
2023-07-26 17:39:57 +0900

    feature: member repository 생성

---
+++ bbiyong-domain/src/main/java/com/capstone/member/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.capstone.member.repository;
+
+
+import com.capstone.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
+}
