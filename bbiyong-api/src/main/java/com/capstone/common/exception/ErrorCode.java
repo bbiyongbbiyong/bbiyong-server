f5ebfcf49402a0a231047698291f6a3f90b63bea
siyeonSon <siyeon0209@naver.com>
2023-07-29 17:05:46 +0900

    refactor: METHOD_NOT_ALLOWED가 발생했을 때 응답 메시지 변경

--- bbiyong-api/src/main/java/com/capstone/common/exception/ErrorCode.java
+++ bbiyong-api/src/main/java/com/capstone/common/exception/ErrorCode.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 public enum ErrorCode {
 
     INTERNAL_SERVER_ERROR(500, "C_001", "서버에 문제가 발생하였습니다."),
-    METHOD_NOT_ALLOWED(405, "C_002", "API는 열려있으나 메소드는 사용 불가합니다."),
+    METHOD_NOT_ALLOWED(405, "C_002", "잘못된 HTTP Request Method 입니다."),
     INVALID_INPUT_VALUE(400, "C_003", "적절하지 않은 요청 값입니다."),
     INVALID_TYPE_VALUE(400, "C_004", "요청 값의 타입이 잘못되었습니다."),
     ENTITY_NOT_FOUND(400, "C_005", "지정한 Entity를 찾을 수 없습니다."),
