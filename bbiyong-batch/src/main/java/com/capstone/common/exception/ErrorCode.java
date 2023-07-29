96ea52bf44617ab67db77e2d5072ac251aa9fff0
siyeonSon <siyeon0209@naver.com>
2023-07-29 22:27:22 +0900

    fix(batch): ErrorCode 추가

--- bbiyong-batch/src/main/java/com/capstone/common/exception/ErrorCode.java
+++ bbiyong-batch/src/main/java/com/capstone/common/exception/ErrorCode.java
@@ -11,8 +11,12 @@ public enum ErrorCode {
     INVALID_TYPE_VALUE(400, "C_004", "요청 값의 타입이 잘못되었습니다."),
     ENTITY_NOT_FOUND(400, "C_005", "지정한 Entity를 찾을 수 없습니다."),
 
-    LOCATION_NOT_FOUND(400, "L_001", "잘못된 위치 정보입니다. 1~17 사이의 숫자를 입력해주세요.")
+    LOCATION_NOT_FOUND(400, "L_001", "잘못된 위치 정보입니다. 1~17 사이의 숫자를 입력해주세요."),
 
+    MEMBER_NOT_FOUND(404, "M_001", "존재하지 않는 사용자입니다."),
+    DUPLICATED_USERNAME(409, "M_002", "이미 존재하는 아이디입니다."),
+    INVALID_PASSWORD(401, "M_003", "비밀번호가 올바르지 않습니다."),
+    INVALID_TOKEN(401, "M_004", "토큰이 만료되었습니다.")
     ;
 
     private final int status;
