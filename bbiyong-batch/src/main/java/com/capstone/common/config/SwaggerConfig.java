607d3890c76083392555a842ed67d1635ebdf8b9
siyeonSon <siyeon0209@naver.com>
2023-07-29 22:23:06 +0900

    fix: batch 모듈에도 swagger 설정 추가

--- bbiyong-batch/src/main/java/com/capstone/common/config/SwaggerConfig.java
+++ bbiyong-batch/src/main/java/com/capstone/common/config/SwaggerConfig.java
@@ -2,12 +2,20 @@ package com.capstone.common.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value(value = "${swagger.server-url}")
+    private String serverUrl;
 
     @Bean
     public GroupedOpenApi publicApi() {
@@ -22,7 +30,13 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .info(new Info().title("삐용삐용 API")
                         .description("삐용삐용 API 명세서입니다.")
-                        .version("v0.0.1"));
+                        .version("v0.0.1"))
+                .servers(List.of(new Server().url(serverUrl)));
+    }
+
+    @Bean
+    public InternalResourceViewResolver defaultViewResolver() {
+        return new InternalResourceViewResolver();
     }
 
 }
