5f1826b02fea6d509feede4eaab416f2234961fc
siyeonSon <siyeon0209@naver.com>
2023-08-12 13:26:28 +0900

    refactor: notification 엔드포인트 변경

--- bbiyong-api/src/main/java/com/capstone/notification/controller/NotificationController.java
+++ bbiyong-api/src/main/java/com/capstone/notification/controller/NotificationController.java
@@ -1,8 +1,10 @@
 package com.capstone.notification.controller;
 
 import com.capstone.common.dto.BasicResponse;
+import com.capstone.member.domain.Member;
 import com.capstone.notification.dto.SubscribeRequestDTO;
 import com.capstone.notification.service.NotificationService;
+import com.capstone.security.annotation.ReqMember;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("notification")
+@RequestMapping("topic")
 @RequiredArgsConstructor
 @Tag(name = "🔔 Notification", description = "알림 구독 설정 API")
 public class NotificationController {
@@ -20,18 +22,20 @@ public class NotificationController {
     private final BasicResponse basicResponse = new BasicResponse();
 
 
-    @PostMapping("/save/topic")
+    @PostMapping("/save")
     @Operation(summary = "알람 토픽 설정", description = "알람 받을 토픽을 설정합니다.")
-    public ResponseEntity<BasicResponse> saveSubscribeTopic(@RequestBody SubscribeRequestDTO requestDTO) {
-        notificationService.saveTopic(requestDTO);
+    public ResponseEntity<BasicResponse> saveSubscribeTopic(
+            @ReqMember Member member,
+            @RequestBody SubscribeRequestDTO requestDTO) {
+        notificationService.saveTopic(member, requestDTO);
         return basicResponse.noContent();
     }
 
-    @GetMapping("/{memberId}/get/topic")
+    @GetMapping("/get")
     @Operation(summary = "알람 토픽 조회", description = "알람 받을 토픽을 조회합니다.")
-    public ResponseEntity<BasicResponse> getSubscribeTopic(@PathVariable("memberId") Long memberId) {
+    public ResponseEntity<BasicResponse> getSubscribeTopic(@ReqMember Member member) {
         return basicResponse.ok(
-                notificationService.getTopic(memberId)
+                notificationService.getTopic(member.getId())
         );
     }
 }
